1aba95e3cc6d145fc79bb804c9220c6d8bb1c5f3
Vishnu Vardhan Vankayalapati <vishnuvankayalapati@giggi.io>
2024-01-09 17:24:07 -0500

    Update firstNonRecurringCharacter.java

--- Day10/firstNonRecurringCharacter.java
+++ Day10/firstNonRecurringCharacter.java
@@ -4,7 +4,7 @@ public class firstNonRecurringCharacter {
         /*
      * 1,2,3,4,1,2,3,5,4 --> 5
      * 2,1,1,2,3,4,3,4,6 --> 6
-     * {} --> Array is Empty!
+     * {} --> Array is Empty!!
      */
 
      public static void Nonrecurring(){
