a792cb8121e3db852e8b824239abb7b1c3ec68d1
Vishnu Vardhan Vankayalapati <vishnuvankayalapati@giggi.io>
2024-01-16 04:46:48 -0500

    Anagram

--- NeetCode/validAnagram.java
+++ NeetCode/validAnagram.java
@@ -1,13 +1,11 @@
 package NeetCode;
 
-import java.util.ArrayList;
-
-import java.util.List;
+import java.util.Arrays;
 
 public class validAnagram {
-    public static void anagram(){
-        String s = "rat";
-        String t = "car";
+    public static boolean anagram(){
+        String s = "anagram";
+        String t = "nagaram";
 
         if (s.length() > t.length()) {
             System.out.println("False");
@@ -16,28 +14,71 @@ public class validAnagram {
         char[] s_char = s.toCharArray();
         char[] t_char = t.toCharArray();
 
-        List<Character> list_s = new ArrayList<>();
-        List<Character> list_t = new ArrayList<>();
+        
 
-        for (char string : s_char) {
-            list_s.add(string);
-            
-        }
+        Arrays.sort(s_char);
+        Arrays.sort(t_char);
+        //int sum_c_char = 0;
+        //int sum_t_char = 0;
+        
 
-        for (char string : t_char) {
-            list_t.add(string);
+        // for (int i = 0; i < s_char.length; i++) {
+        //     char Character = s_char[i];
+        //     // System.out.println(Character);
+        //     ascii = (int) Character;
+        //     sum_c_char = ascii + sum_c_char;
             
-        }
+        // }  System.out.println("s_char: " + sum_c_char);
+
+        // for (int i = 0; i < t_char.length; i++) {
+        //     char Character = t_char[i];
+        //     // System.out.println(Character);
+        //     ascii = (int) Character;
+        //     sum_t_char = ascii + sum_t_char;
+            
+        // } System.out.println("t_char: " + sum_t_char);
+
+        // if (sum_c_char == sum_t_char) {
+        //     return true;
+        // }
+        // return false;
+
+        // System.out.println(ascii);
+
+        // List<Character> list_s = new ArrayList<>();
+        // List<Character> list_t = new ArrayList<>();
+
+        // for (char string : s_char) {
+        //     list_s.add(string);
+            
+        // }
+
+        // for (char string : t_char) {
+        //     list_t.add(string);
+            
+        // }
+
+        // System.out.println(list_s);
+
+
  
-        if (list_s.containsAll(list_t)) {
-            System.out.println("True");
-        } else {
-            System.out.println("False");
+        // if (list_s.containsAll(list_t)) {
+        //     System.out.println("True");
+        // } else {
+        //     System.out.println("False");
+        // }
+        if (Arrays.equals(s_char, t_char)) {
+            return true;
         }
 
+        return false;
         
     }
     public static void main(String[] args) {
-        anagram();
+        boolean isAnagram = anagram();
+        System.out.println("Is Anagram?: " + isAnagram);
+        
+        
+        
     }
 }
