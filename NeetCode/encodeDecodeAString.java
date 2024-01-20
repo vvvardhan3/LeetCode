68b82e94f1f8172d496c9af6a989ec314b80d9db
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-01-20 01:09:44 -0500

    Update encodeDecodeAString.java

--- NeetCode/encodeDecodeAString.java
+++ NeetCode/encodeDecodeAString.java
@@ -6,19 +6,35 @@ public class encodeDecodeAString {
     public static void encode(){
         String[] input = {"Vishnu","Vardhan"};
         String result = " ";
+        
+        String finalOutput = "";
 
+        
 
         for (String string : input) {
-            result = string.length() + "[" + string + "]";
-            System.out.println(result);
-        }
+            String temp = string.length() + "[" + string + "]";
+            result = result + temp;
+        } System.out.println(result);
+        
 
         Stack<Integer> intstack = new Stack<>();
-        Stack<String> stringstack = new Stack<>();
+        Stack<Character> stringstack = new Stack<>();
 
-        for (Character c : result.toCharArray()) {
-            System.out.println(c);
-        }
+        char[] charresult = result.toCharArray();
+
+        for (char c : charresult) {   
+            if (Character.isDigit(c)) {
+                intstack.push(Character.getNumericValue(c));
+            } else {
+                stringstack.push(c);
+            }
+        } 
+        System.out.println(intstack);
+        System.out.println(stringstack);
+
+        
+        /* Need to Work on this code more!! */
+
 
 
     }
