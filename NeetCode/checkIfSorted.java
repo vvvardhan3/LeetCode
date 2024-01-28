c4717d9956671e1a6b68fa2b81781159aeb0ddd0
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-01-28 15:23:55 -0500

    Update checkIfSorted.java

--- NeetCode/checkIfSorted.java
+++ NeetCode/checkIfSorted.java
@@ -2,19 +2,41 @@ package NeetCode;
 
 public class checkIfSorted {
     public static boolean sorted(){
-        int nums[] ={1,1,1,1};
+        int nums[] ={3};
         int n = nums.length;
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[(i+1)%n] < nums[i]) {
-                count++;
+        int start = nums[0];
+        int end = nums[n-1];
+
+        if (end > start) {
+            return false;
+        } else {
+
+            int largestElement = nums[0];
+            int largestElementIndex = 0;
+            for (int i = 0; i < n; i++) {
+                if (nums[i]>largestElement) {
+                    largestElement = nums[i];
+                    largestElementIndex = i;
+                }
             } 
-            if (count > 1) {
-                return false;
-            }
-        } return true;
+                System.out.println("Largest element in the array: "+largestElement);
+                System.out.println( "Index of Largest element: "+ largestElementIndex);
+
+            for (int i = largestElementIndex -1 ; i >= 0; i--) {
+                for (int j = largestElementIndex + 1 ; j < n; j++) {
+                    if ((nums[j] < nums[j+1]) && (nums[i] > nums[i-1]) &&
+                    (nums[largestElementIndex-1] < nums[largestElementIndex]) && (nums[largestElementIndex + 1] < nums[largestElementIndex])) {
+                    // nums[1] > nums[0]
+                    return true;
+                }    
+            }   
+        }
+        }
+        return false;
+
     }
     public static void main(String[] args) {
         System.out.println("Is sorted:? " + sorted());
+        // sorted();
     }
 }
