1edcfb6a535dcc6f8929a5e8ec9b012c5ab83592
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-01-28 15:41:58 -0500

    Update checkIfSorted.java

--- NeetCode/checkIfSorted.java
+++ NeetCode/checkIfSorted.java
@@ -2,36 +2,23 @@ package NeetCode;
 
 public class checkIfSorted {
     public static boolean sorted(){
-        int nums[] ={3};
+        int nums[] ={5,1,2,3,4};
         int n = nums.length;
-        int start = nums[0];
-        int end = nums[n-1];
+        int count = 0;
 
-        if (end > start) {
-            return false;
-        } else {
-
-            int largestElement = nums[0];
-            int largestElementIndex = 0;
-            for (int i = 0; i < n; i++) {
-                if (nums[i]>largestElement) {
-                    largestElement = nums[i];
-                    largestElementIndex = i;
-                }
-            } 
-                System.out.println("Largest element in the array: "+largestElement);
-                System.out.println( "Index of Largest element: "+ largestElementIndex);
-
-            for (int i = largestElementIndex -1 ; i >= 0; i--) {
-                for (int j = largestElementIndex + 1 ; j < n; j++) {
-                    if ((nums[j] < nums[j+1]) && (nums[i] > nums[i-1]) &&
-                    (nums[largestElementIndex-1] < nums[largestElementIndex]) && (nums[largestElementIndex + 1] < nums[largestElementIndex])) {
-                    // nums[1] > nums[0]
-                    return true;
-                }    
-            }   
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i-1] > nums[i]) {
+                count++;
+            }
+        } 
+        if (nums[n-1] > nums[0]) {
+            count++;
         }
+
+        if (count <= 1) {
+            return true;
         }
+        
         return false;
 
     }
