30f1b49962088c72d17344675e3714364a52b028
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-01-28 19:31:20 -0500

    move Zeros

--- NeetCode/moveZeros.java
+++ NeetCode/moveZeros.java
@@ -1,30 +1,38 @@
 package NeetCode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class moveZeros {
     public static void zeros(){
         int[] nums = {0,1,0,3,12};
         int n = nums.length;
-        List<Integer> list = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
+        // int az = 0;
+        // int z = 0;
+        
+        // while (az < n) {
+        //     if (nums[az] != 0) {
+        //         int temp = nums[az];
+        //         nums[az] = nums[z];
+        //         nums[z] = temp;
+        //         az++;
+        //         z++;
+        //     } else {
+        //         az++;
+        //     }
+        // }
+        // for (int i : nums) {
+        //     System.out.println(i);
+        // }
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
-                list.add(nums[i]);
-            }
+                nums[count++] = nums[i];
+            }    
+        }
+        while (count < nums.length) {
+            nums[count++] = 0;
         }
 
-        for (int i = 0; i < list.size(); i++) {
-            nums[i] = list.get(i);
-        }
-
-        for (int i = list.size(); i < n; i++) {
-            nums[i] = 0;
-        }
-
-        for (Integer integer : nums) {
-            System.out.println(integer);
+        for (int i : nums) {
+            System.out.println(i);
         }
 
 
