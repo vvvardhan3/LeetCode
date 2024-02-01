9884fd9ee438874d594a00cbe27cef6070279423
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-02-01 17:43:39 -0500

    Create findFirstAndLastPositionOfElmentInSortedArray.java

---
+++ binarySearch/findFirstAndLastPositionOfElmentInSortedArray.java
@@ -0,0 +1,15 @@
+package binarySearch;
+
+public class findFirstAndLastPositionOfElmentInSortedArray {
+    public static void position(){
+        int nums[] = {5,7,7,8,8,10};
+        int target = 8;
+
+        
+        
+
+    }
+    public static void main(String[] args) {
+        position();
+    }
+}
