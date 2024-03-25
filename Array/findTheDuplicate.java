1a97a9d865569e843a74361ed6accadcfc7bd642
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-03-25 15:42:09 -0400

    Update findTheDuplicate.java

--- Array/findTheDuplicate.java
+++ Array/findTheDuplicate.java
@@ -2,6 +2,17 @@ package Array;
 
 public class findTheDuplicate {
     public static void find(){
+        int[] nums = {3,1,3,4,2};
+
+      
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    System.out.println(nums[i]);
+                }
+            }
+        }
 
     }
     public static void main(String[] args) {
